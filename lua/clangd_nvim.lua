39eb6e6c4731d408e12fadabe794448491ed7161
Robert Oleynik <robert.oleynik@t-online.de>
2020-06-12 10:46:10 +0200

    Fix Lua Includes

--- lua/clangd_nvim.lua
+++ lua/clangd_nvim.lua
@@ -20,7 +20,7 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 -- SOFTWARE.
 
-local base64 = require'clangd_nvim/base64'
+local base64 = require'clangd_nvim/base64_decode'
 local highlight = require'vim/highlight'
 
 local clangd_scopes = {}
